8d491419a91e61f504f844a5db9012c64b1b4d91
MrSingh529 <harpinder.singh@rvsolutions.in>
2025-07-22 10:13:14 +0530

    fixing deployment

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,6 +4,8 @@ import { AdminClient } from './admin-client';
 import { AuthGuard } from '@/components/auth-guard';
 import { Skeleton } from '@/components/ui/skeleton';
 
+export const dynamic = 'force-dynamic';
+
 export default function AdminDashboard() {
   return (
     <AuthGuard>
