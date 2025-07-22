812c9bee13de9b0912794ba6abbaa7393877e5dc
MrSingh529 <harpinder.singh@rvsolutions.in>
2025-07-22 10:17:09 +0530

    fixing deployment

--- src/app/track/page.tsx
+++ src/app/track/page.tsx
@@ -1,6 +1,10 @@
+
 import { Suspense } from 'react';
 import { TrackingClient } from './tracking-client';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+
+export const dynamic = 'force-dynamic';
 
 export default function TrackPage() {
   return (
@@ -23,9 +27,17 @@ export default function TrackPage() {
 
 function CardSkeleton() {
     return (
-        <div className="space-y-4">
-            <Skeleton className="h-24 w-full" />
-            <Skeleton className="h-48 w-full" />
-        </div>
+        <Card>
+            <CardHeader>
+                <Skeleton className="h-6 w-1/3" />
+                <Skeleton className="h-4 w-2/3" />
+            </CardHeader>
+            <CardContent>
+                <div className="flex w-full items-center space-x-2">
+                    <Skeleton className="h-10 flex-grow" />
+                    <Skeleton className="h-10 w-10" />
+                </div>
+            </CardContent>
+        </Card>
     )
 }
